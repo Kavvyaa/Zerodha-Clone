114ccec10fc2ebddaf50504144f8b51d6da34d3d
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-07-14 23:26:58 +0530

    frontend updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import HomePage from './landingPage/home/HomePage';
-import Signup from './landingPage/signup/Signup';
+import Signup from './landingPage/signup/signup';
 import AboutPage from './landingPage/about/AboutPage';
 import ProductsPage from './landingPage/products/ProductsPage';
 import PricingPage from './landingPage/pricing/PricingPage';
