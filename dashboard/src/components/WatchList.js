61681a572d4551309d71a4ee6c4c68fffa19f710
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-07-16 23:46:10 +0530

    watchlist

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
-import {Tooltip, Grow} from '@mui/material';
+import GeneralContext from "./GeneralContext";
+
+import { Tooltip, Grow } from "@mui/material";
+
+import {
+  BarChartOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreHoriz,
+} from "@mui/icons-material";
 
 import { watchlist } from "../data/data";
-
-import {KeyboardArrowUp, KeyboardArrowDown, BarChartOutlined, MoreHoriz} from '@mui/icons-material';
 
 const WatchList = () => {
   return (
@@ -21,9 +28,8 @@ const WatchList = () => {
       </div>
 
       <ul className="list">
-        {watchlist.map((stock, index)=>{
-          return (
-          <WatchListItem stock={stock} key={index} />)
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
         })}
       </ul>
     </div>
@@ -32,49 +38,79 @@ const WatchList = () => {
 
 export default WatchList;
 
-const WatchListItem = ({stock})=>{
+const WatchListItem = ({ stock }) => {
   const [showWatchlistActions, setShowWatchlistActions] = useState(false);
 
-  const handleMouseEnter = (e)=>{
+  const handleMouseEnter = (e) => {
     setShowWatchlistActions(true);
-  }
+  };
 
-    const handleMouseExit = (e)=>{
+  const handleMouseLeave = (e) => {
     setShowWatchlistActions(false);
-  }
+  };
 
   return (
-    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="item">
         <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
         <div className="itemInfo">
           <span className="percent">{stock.percent}</span>
-          {stock.isDown ? (<KeyboardArrowDown className="down" />) : (<KeyboardArrowUp className="up" />)}
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="down" />
+          )}
           <span className="price">{stock.price}</span>
         </div>
       </div>
-      {showWatchlistActions && <WatchListActions uuid={stock.name}/>}
+      {showWatchlistActions && <WatchListActions uid={stock.name} />}
     </li>
   );
 };
 
-const WatchListActions = ({uuid})=>{
+const WatchListActions = ({ uid }) => {
+  const generalContext = useContext(GeneralContext);
+
+  const handleBuyClick = () => {
+    generalContext.openBuyWindow(uid);
+  };
+
   return (
     <span className="actions">
       <span>
-        <Tooltip title="Buy (B)" placement="top" arrow TransitionComponent={Grow}>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+          onClick={handleBuyClick}
+        >
           <button className="buy">Buy</button>
         </Tooltip>
-        <Tooltip title="Sell (S)" placement="top" arrow TransitionComponent={Grow}>
+        <Tooltip
+          title="Sell (S)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
           <button className="sell">Sell</button>
         </Tooltip>
-        <Tooltip title="Analytics (A)" placement="top" arrow TransitionComponent={Grow}>
-          <button className="action"><BarChartOutlined className="icon"/></button>
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
         </Tooltip>
         <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
-          <button className="action"><MoreHoriz className="icon"/></button>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
         </Tooltip>
       </span>
     </span>
-  )
-}
+  );
+};
