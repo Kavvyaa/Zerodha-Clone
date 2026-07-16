ff7959889ed707b1eebf9f49d8c25251e5880c51
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-07-16 23:06:49 +0530

    data from DB

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const {HoldingsModel} = require('./models/HoldingsModel');
 const {PositionsModel} = require('./models/PositionsModel');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3002;
 
