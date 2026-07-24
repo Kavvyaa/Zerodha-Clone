4d52a3808b5259c4573250b30fd8fd456547d5e8
Kavvyaa <157046519+Kavvyaa@users.noreply.github.com>
2026-07-24 12:03:13 +0530

    ready to deploy

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://YOUR-FRONTEND.netlify.app",
-  "https://YOUR-DASHBOARD.netlify.app",
+  "https://my-zerodh.netlify.app/",
+  "https://zerodha-dashboar.netlify.app/",
 ];
 
 app.use(
